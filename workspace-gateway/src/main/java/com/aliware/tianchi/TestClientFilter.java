39bd7f6a7d66954ed04e1597001a473a8a49a1c7
huaran <huaran.fan@dmall.com>
2019-07-30 18:00:45 +0800

    设置超时时间，唉

--- workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
@@ -3,10 +3,17 @@ package com.aliware.tianchi;
 import com.aliware.tianchi.metric.InvokerMetric;
 import com.aliware.tianchi.metric.RelativeLeapWindowInvokerMetricImpl;
 import org.apache.dubbo.common.Constants;
+import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.common.utils.StringUtils;
 import org.apache.dubbo.rpc.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author daofeng.xjf
@@ -33,7 +40,8 @@ public class TestClientFilter implements Filter {
             // 起始时间放入上下文缓存当中
             RpcContext.getContext().setAttachment("invoke_start", String.valueOf(start));
             // 设置超时时间
-            RpcContext.getContext().setAttachment(Constants.TIMEOUT_KEY, String.valueOf(DEFAULT_TIME_OUT));
+//            RpcContext.getContext().setAttachment(Constants.TIMEOUT_KEY, String.valueOf(DEFAULT_TIME_OUT));
+            setTimeout(invoker.getUrl(), invocation, String.valueOf(DEFAULT_TIME_OUT));
             // 执行远程调用，注意调用是异步执行
             Result result = invoker.invoke(invocation);
 
@@ -60,5 +68,41 @@ public class TestClientFilter implements Filter {
         metric.invokeEnd(invoker, costTime);
 
         return result;
+    }
+
+    private void setTimeout(URL url, Invocation invocation, String timeout){
+        String key = invocation.getMethodName() + "." + Constants.TIMEOUT_KEY;
+        putParameter(url, key, timeout);
+    }
+
+    public static void putParameter(URL url, String key, String value){
+        try {
+            if(StringUtils.isEquals(url.getParameter(key),value)){
+                return;
+            }
+
+            Field field = URL.class.getDeclaredField("parameters");
+            field.setAccessible(true);
+
+            updateUnmodifiedMap((Map) field.get(url), key, value);
+            Method method = URL.class.getDeclaredMethod("buildString", boolean.class, boolean.class, String[].class);
+            method.setAccessible(true);
+            method.invoke(url, false, true, null);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateUnmodifiedMap(Map unmodifiedMap, String key, String value) throws Exception {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        for(Class cl : classes) {
+            if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                Field field = cl.getDeclaredField("m");
+                field.setAccessible(true);
+                Object obj = field.get(unmodifiedMap);
+                Map map = (Map) obj;
+                map.put(key, value);
+            }
+        }
     }
 }
