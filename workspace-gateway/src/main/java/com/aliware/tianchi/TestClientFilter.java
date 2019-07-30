e81f25c63f4e71b19dc7a3207c9bd47a1a63650d
huaran <huaran.fan@dmall.com>
2019-07-30 17:18:46 +0800

    设置超时时间

--- workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
@@ -33,8 +33,7 @@ public class TestClientFilter implements Filter {
             // 起始时间放入上下文缓存当中
             RpcContext.getContext().setAttachment("invoke_start", String.valueOf(start));
             // 设置超时时间
-            RpcContext.getContext().setAttachment(Constants.TIMEOUT_KEY, String.valueOf(DEFAULT_TIME_OUT));
-            RpcContext.getContext().set(Constants.TIMEOUT_KEY, String.valueOf(DEFAULT_TIME_OUT));
+            invoker.getUrl().addParameter(Constants.TIMEOUT_KEY, DEFAULT_TIME_OUT);
             // 执行远程调用，注意调用是异步执行
             Result result = invoker.invoke(invocation);
 
@@ -49,12 +48,12 @@ public class TestClientFilter implements Filter {
     public Result onResponse(Result result, Invoker<?> invoker, Invocation invocation) {
         long costTime = -1;
 
-        if (result.getException() == null){
+        if (result.getException() == null) {
             // 记录结束时间
             long end = System.currentTimeMillis();
             // 从上下文中拿出起始时间
             long start = Long.valueOf(RpcContext.getContext().getAttachment("invoke_start"));
-            costTime = end -start;
+            costTime = end - start;
         }
 
         // 埋点
